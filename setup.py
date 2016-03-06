b40360d9b0a6002b37247ba53b5e1bc3cfc24e1a
hzsunshx <hzsunshx@corp.netease.com>
2016-03-06 20:34:25 +0800

    remove useless commment

--- setup.py
+++ setup.py
@@ -2,25 +2,3 @@
 
 import setuptools
 setuptools.setup(setup_requires=['pbr'], pbr=True)
-
-# from __future__ import print_function
-
-# import aircv
-
-# #try: from distutils.core import setup
-# #except ImportError: from setuptools import setup
-# from setuptools import setup
-
-# setup(
-#       name='aircv',
-#       version=aircv.__version__,
-#       license="MIT",
-#       description='Image utils based on python-opencv2',
-
-#       author='codeskyblue',
-#       author_email='codeskyblue@gmail.com',
-#       url='http://github.com/netease/aircv',
-
-#       py_modules=['aircv'],
-#       install_requires=[],
-# )
